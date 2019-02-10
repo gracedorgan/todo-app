6fc6fc0fd165099c4c46f882422d2bd7b7dc804e
Grace <Grace.M.Dorgan.21@dartmouth.edu>
2019-02-10 17:37:38 -0500

    delete functionality

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
+import useInputState from './useInputState';
+
 const TodoForm = ({ saveTodo }) => {
-  const [value, setValue] = useState('');
+  const { value, reset, onChange } = useInputState('');
+
   return (
     <form
-        onSubmit={event => {
+      onSubmit={event => {
         event.preventDefault();
+
         saveTodo(value);
-      }}>
+        reset();
+      }}
+    >
       <TextField
         variant="outlined"
         placeholder="Add todo"
         margin="normal"
-        onChange={event => {
-          setValue(event.target.value);
-        }}
+        onChange={onChange}
         value={value}
       />
     </form>
   );
 };
+
 export default TodoForm;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
 import Typography from '@material-ui/core/Typography';
-import './styles.css';
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import useTodoState from './useTodoState';
+import './styles.css';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
 
-      <  saveTodo={todoText => {
-        const trimmedText = todoText.trim();
-        if (trimmedText.length > 0) {
-          setTodos([...todos, trimmedText]);
-    }
-  }} />
-      <TodoList todos={todos} />
+      <TodoForm
+        saveTodo={todoText => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 };
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
